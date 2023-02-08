78b2015fc57695b10ff3bb2c74d1b76885a8aba4
Ignatev Nikita <nikitaignatiev@gmail.com>
2023-02-08 23:00:16 +0300

    Trying to add static path

--- config/urls.py
+++ config/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('mog/', include(mog_urls)),
     path('api-auth/', include('rest_framework.urls')),
-    # *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
 
 # Для статики:
